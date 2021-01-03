e8ddd7ab0649c0e400da5fbcf39a06814644f50c
pwdz <me.adibzadeh@gmail.com>
2021-01-04 00:42:46 +0330

    Implement list devs

--- cmd/list.go
+++ cmd/list.go
@@ -15,23 +15,20 @@
 package cmd
 
 import (
-	"fmt"
-
+	// "fmt"
+	"github.com/pwdz/WirePenguin/pkg/list"
 	"github.com/spf13/cobra"
 )
 
 // listCmd represents the list command
 var listCmd = &cobra.Command{
 	Use:   "list",
-	Short: "A brief description of your command",
-	Long: `A longer description that spans multiple lines and likely contains examples
-and usage of using your command. For example:
+	Short: "TODO",
+	Long: `TODO`,
 
-Cobra is a CLI library for Go that empowers applications.
-This application is a tool to generate the needed files
-to quickly create a Cobra application.`,
 	Run: func(cmd *cobra.Command, args []string) {
-		fmt.Println("list called")
+		// fmt.Println("list called")
+		list.ListDevices()
 	},
 }
 
---
+++ pkg/list/list.go
@@ -0,0 +1,27 @@
+package list
+
+import (
+	// "github.com/google/gopacket"
+	"fmt"
+	"log"
+
+	"github.com/google/gopacket/pcap"
+)
+func ListDevices() {
+	// var devices pcap.
+	devices, err := pcap.FindAllDevs()
+	if err != nil{
+		log.Fatal(err)
+	}
+
+	fmt.Println("Interfaces:")
+	for _, dev := range devices{
+		fmt.Println("Name:", dev.Name)
+		fmt.Println("Description:", dev.Description)
+		fmt.Println("Address:", dev.Addresses)
+		// fmt.Println("Flags:", dev.Flags)
+		fmt.Println("================================================")
+	}
+
+
+}
